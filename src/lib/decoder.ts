b636102383979df7c394d1db5b79cad1d8dc869f
Ayush Saini <ayushsainiiitb00@gmail.com>
2025-09-27 14:30:05 +0530

    validating sub-fields

--- src/lib/decoder.ts
+++ src/lib/decoder.ts
@@ -67,10 +67,12 @@ export const validateCron = (cronExpr: string): boolean => {
   // now that specials are checked
   if (parts.length !== 5) return false;
 
-  parts.forEach((part, idx) => {
+  for (let idx = 0; idx < parts.length; idx++) {
+    const part = parts[idx]!; // Non-null assertion
+
     const type = getFieldType(idx);
     if (!validateField(part, type)) return false;
-  });
+  }
 
   return true;
 };
@@ -121,28 +123,138 @@ export const validateField = (
   const tokens = fieldValue.split(",");
   for (const token of tokens) {
     if (token.includes("/")) {
-      if (!validateStep(token, lowerAllowedRange, upperAllowedRange))
+      if (
+        !validateStep(
+          token,
+          lowerAllowedRange,
+          upperAllowedRange,
+          allowedSingleValues
+        )
+      )
         return false;
     } else if (token.includes("-")) {
-      if (!validateRange(token, lowerAllowedRange, upperAllowedRange))
+      if (
+        !validateRange(
+          token,
+          lowerAllowedRange,
+          upperAllowedRange,
+          allowedSingleValues
+        )
+      )
         return false;
     } else {
-      if (!validateNum(token, lowerAllowedRange, upperAllowedRange))
+      if (
+        !validateNum(token, lowerAllowedRange, upperAllowedRange) &&
+        !allowedSingleValues?.includes(token.toUpperCase())
+      )
         return false;
     }
   }
 
+  return true;
+};
+
+// */n -> every n units
+// a-b/n -> every n units, from a through b
+// x/n -> every n units, from x through MAX_OF_RANGE
+const validateStep = (
+  token: string,
+  min: number,
+  max: number,
+  allowedSingleValues: string[] | null
+): boolean => {
+  const parts = token.split("/");
+  if (parts.length !== 2) return false;
+
+  const [first, second] = parts;
+  if (!first || !second) return false;
+
+  // second must either be a positive integer (greator than 0)
+  // or any of the allowed single value for supported fieldTypes
+  const secondNum = Number(second);
+  const isPositiveInt =
+    secondNum && secondNum > 0 && Number.isInteger(secondNum);
+  const isAllowedVal = allowedSingleValues?.includes(second.toUpperCase());
+
+  if (!isPositiveInt && !isAllowedVal) return false;
+
+  // now that we have made sure that second is valid, we can check firstz
+  if (first === "*") return true;
+
+  if (first.includes("-")) {
+    return validateRange(first, min, max, allowedSingleValues);
+  }
+
+  const firstNum = Number(first);
+  if (Number.isInteger(firstNum)) {
+    return validateNum(first, min, max);
+  }
+
+  if (allowedSingleValues?.includes(first.toUpperCase())) return true;
+
   return false;
 };
 
-const validateStep = (token: string, min: number, max: number): boolean => {
-  return false;
-};
+// a-b -> from a through b, in case of numbers b >= a,
+// in case of allowed single values, same rule will be followed by checking their respective indexes
+// for example:
+// fri-4 : this is invalid because idx of fri is 5 && 5 > 4
 
-const validateRange = (token: string, min: number, max: number): boolean => {
-  return false;
+const validateRange = (
+  token: string,
+  min: number,
+  max: number,
+  allowedSingleValues: string[] | null
+): boolean => {
+  const parts = token.split("-");
+  if (parts.length !== 2) return false;
+
+  const [first, second] = parts;
+  if (!first || !second) return false;
+
+  // both should either be integers in allowed ranges
+  // or allowed single values
+  // and second >= first
+  const isFirstValid =
+    isInt(first) || allowedSingleValues?.includes(first.toUpperCase());
+  const isSecondValid =
+    isInt(second) || allowedSingleValues?.includes(second.toUpperCase());
+
+  if (!isFirstValid && !isSecondValid) return false;
+
+  // these would either be the literal number values or indices of allowed single values
+  let firstIdx = Number(first),
+    secondIdx = Number(second);
+
+  if (allowedSingleValues?.includes(first.toUpperCase())) {
+    const i = allowedSingleValues?.findIndex(
+      (el) => el === first.toUpperCase()
+    );
+    if (i !== -1) firstIdx = i;
+  }
+  if (allowedSingleValues?.includes(second.toUpperCase())) {
+    const i = allowedSingleValues?.findIndex(
+      (el) => el === second.toUpperCase()
+    );
+    if (i !== -1) secondIdx = i;
+  }
+
+  return firstIdx >= min && secondIdx <= max && secondIdx >= firstIdx;
 };
 
 const validateNum = (token: string, min: number, max: number): boolean => {
-  return false;
+  const num = Number(token);
+  if (!Number.isInteger(num)) return false;
+
+  return num >= min && num <= max;
 };
+
+const isInt = (val: string) => {
+  const num = Number(val);
+  return Number.isInteger(num);
+};
+
+// const isPositiveInt = (val: string) => {
+//   const num = Number(val);
+//   return isInt(val) && num > 0;
+// };
